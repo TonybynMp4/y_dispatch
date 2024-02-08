ba1cf382de730f4242c65bdd8a4565cae270f66c
Antony <97451137+TonybynMp4@users.noreply.github.com>
2024-02-08 17:24:47 +0100

    bunch of small changes (#15)
    * feat: .editorconfig file
    
    * fix: version 1.4.0
    
    * fix: qbx_dispatch
    
    * feat: use NPWD by default

--- config/server.lua
+++ config/server.lua
@@ -1,4 +1,4 @@
 return {
-    useNpwd = false, -- Use npwd for 911/912 calls (send a text to the number and the message is sent to the dispatch)
+    useNpwd = true, -- Use npwd for 911/912 calls (send a text to the number and the message is sent to the dispatch)
     versionCheck = true -- Get notified when a new version is available on GitHub
 }
