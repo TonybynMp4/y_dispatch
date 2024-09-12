a107ba73537b6637439c585d7f7df08c4d712932
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-09-12 10:15:19 +0200

    move calls to it's own config file & make it serverside (logic not done ofc)

--- config/server.lua
+++ config/server.lua
@@ -1,4 +1,6 @@
 return {
     useNpwd = true, -- Use npwd for 911/912 calls (send a text to the number and the message is sent to the dispatch)
-    versionCheck = true -- Get notified when a new version is available on GitHub
+    versionCheck = true, -- Get notified when a new version is available on GitHub
+
+    calls = require('config.dispatchCalls')
 }
