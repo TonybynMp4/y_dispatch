069ad149a51b6731d7ea9531aabf20f632c54054
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-09-30 09:41:48 +0200

    fix: check if the player should be able to use these commands or not & new function

--- server/main.lua
+++ server/main.lua
@@ -5,15 +5,42 @@ if config.versionCheck then
 	lib.versionCheck('TonybynMp4/y_dispatch')
 end
 
+function HasDispatchEnabledGroup(playerJobs)
+    if not playerJobs then return false end
+
+    if playerJobs.name then
+        if dispatchJobs.jobs[playerJobs.name].dispatch or dispatchJobs.types[playerJobs.type].dispatch then
+            return true
+        else
+            return false
+        end
+    else
+        for i = 1, #playerJobs do
+            if dispatchJobs.jobs[playerJobs[i].name].dispatch or dispatchJobs.types[playerJobs[i].type].dispatch then
+                return true
+            end
+        end
+    end
+
+    return false
+end
+
 lib.addCommand('mutedispatch', {help = locale('commands.mutedispatch')}, function(source, _)
-    Player(source).state:set('dispatchMuted', not Player(source).state.dispatchMuted, true)
-    exports.qbx_core:Notify(source, locale('success.dispatch' .. ((Player(source).state.dispatchMuted and 'Muted') or 'Unmuted')), 'inform')
-    exports.qbx_core:Save(source)
+    local PlayerData = exports.qbx_core:GetPlayer(source).PlayerData
+    if HasDispatchEnabledGroup(PlayerData.job) then
+        return exports.qbx_core:Notify(source, locale('error.noPermission'), 'error')
+    end
+
+    local playerState = Player(source).state
+    Player(source).state:set('dispatchMuted', not playerState.dispatchMuted, true)
+    exports.qbx_core:Notify(source, locale('success.dispatch' .. ((playerState.dispatchMuted and 'Muted') or 'Unmuted')), 'inform')
 end)
 
 lib.addCommand('disabledispatch', {help = locale('commands.disabledispatch')}, function(source, _)
     local job = exports.qbx_core:GetPlayer(source).PlayerData.job
-    if not dispatchJobs.Types[job.type] or not dispatchJobs.Jobs[job.name] then return end
+    if not HasDispatchEnabledGroup(job) then
+        return exports.qbx_core:Notify(source, locale('error.noPermission'), 'error')
+    end
 
     TriggerClientEvent('y_dispatch:client:DisableDispatch', source)
 end)
