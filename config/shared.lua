d74051f2df479a9ecdf60ac74ea61316e525c073
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-09-30 09:40:51 +0200

    fix: better config, might move to server only if possible

--- config/shared.lua
+++ config/shared.lua
@@ -1,15 +1,42 @@
 return {
-    -- Jobs that uses the dispatch system
+    -- Jobs that uses the dispatch system & their rights
+    -- the "dispatch" key is what lets you use the dispatch system at all
+    -- the "bolos" key is what lets you see and use the BOLOs tab
+    --TODO: the "bracelets" key is what lets you see and use the bracelets tab
+    --TODO: the "panic" key is what lets you use the panic button
+    ---@type dispatchJobs
     dispatchJobs = {
-        Jobs = {
-            police = true,
-            sheriff = true,
-            ambulance = true
+        jobs = {
+            police = {
+                dispatch = true,
+                bolos = true,
+                bracelets = true,
+                panic = true
+            },
+            sheriff = {
+                dispatch = true,
+                bolos = true,
+                bracelets = true,
+                panic = true
+            },
+            ambulance = {
+                dispatch = true,
+                bolos = true,
+                panic = true,
+            },
         },
-        Types = {
-            leo = true,
-            medic = true,
-            ems = true
+        types = {
+            leo = {
+                dispatch = true,
+                bolos = true,
+                bracelets = true,
+                panic = true
+            },
+            medic, ems = {
+                dispatch = true,
+                bolos = true,
+                panic = true,
+            },
         }
     },
 }
