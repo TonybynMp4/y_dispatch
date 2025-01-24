549c58153eaf97a22ae53d6bb735a761614041af
Tony <97451137+TonybynMp4@users.noreply.github.com>
2025-01-25 00:08:49 +0100

    fix: remove unused searchRef prop and update placeholder handling in SearchBar component

--- web/src/components/shadcn/searchbar.tsx
+++ web/src/components/shadcn/searchbar.tsx
@@ -1,15 +1,14 @@
 import { Input } from "./ui/input";
 
 type Props = {
-    searchRef?: React.RefObject<HTMLInputElement> | undefined;
-    placeHolder: string | undefined;
+    placeHolder?: string;
     onchange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     onkeyup?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
     className?: string;
     inputClassName?: string;
 }
 
-function SearchBar({searchRef, placeHolder, onchange, onkeyup, className, inputClassName }: Props) {
+function SearchBar({ placeHolder, onchange, onkeyup, className, inputClassName }: Props) {
     return (
         <div className={"relative flex items-center " + className}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
@@ -17,7 +16,7 @@ function SearchBar({searchRef, placeHolder, onchange, onkeyup, className, inputC
             className="absolute left-5 h-4 w-4 text-muted-foreground">
                 <circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path>
             </svg>
-            <Input ref={searchRef} onKeyUp={onkeyup} onChange={onchange} className={"h-[2.5%] w-full p-4 pl-[2.5rem] text-l "  + inputClassName} placeholder={placeHolder || "Search"} />
+            <Input onKeyUp={onkeyup} onChange={onchange} className={"h-[2.5%] w-full p-4 pl-[2.5rem] text-l "  + inputClassName} placeholder={placeHolder || "Search"} />
         </div>
     );
 }
